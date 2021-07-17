6b7fd2b0f7d9f87bbfc319bd920066c62e1ead2d
Fuwn <fuwnzy@gmail.com>
2021-07-17 19:40:23 +0000

    fix(content): file reading error message

--- content.go
+++ content.go
@@ -9,8 +9,7 @@ func GetContent(file string) string {
 	if err != nil {
 		return `# ERROR READING FILE
 
-I seem to be having some trouble reading the contents of the "` + file + `" file... Try refreshing the page and if the problem persists, contact Fuwn! (Contact information available at /contact)
-`
+I seem to be having some trouble reading the contents of the "` + file + `" file... Try refreshing the page and if the problem persists, contact Fuwn!`
 	}
 
 	return string(contents)
